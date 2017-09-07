8801179a7d223202b8cb1a82a21c847f269cf975
LilianCunanan <31613473+LilianCunanan@users.noreply.github.com>
2017-09-07 17:51:44 +0800

    Update IAT.java

--- IAT.java
+++ IAT.java
@@ -1,32 +1,30 @@
 interface IAT {}
 
 class Unknown implements IAT{
-	Unknown(){}
+  Unknown(){}
 }
 
 class Human implements IAT{
-	String name;
-	IAT mom;
-	IAT dad;
+  String name;
+  IAT mom;
+  IAT dad;
 	
-	Human(String name,IAT mom,IAT dad){
-		this.name = name;
-		this.mom = mom;
-		this.dad = dad;
-	}
+  Human(String name,IAT mom,IAT dad){
+    this.name = name;
+    this.mom = mom;
+    this.dad = dad;
+  }
 }
 
 class ExamplesAncestors{
-	ExamplesAncestors(){}
+  ExamplesAncestors(){}
 	
-	IAT unknown = new Unknown();
-	IAT catalina = new Human("Catalina",this.unknown,this.unknown);
-	IAT pedro = new Human("Pedro",this.unknown,this.unknown);
-	IAT magdalena = new Human("Magdalena",this.unknown,this.unknown);
-	IAT jose = new Human("Jose",this.unknown,this.unknown);
-	
-	IAT marilyn = new Human("Marilyn",this.catalina,this.pedro);
-	IAT carlos = new Human("Carlos",this.magdalena,this.jose);
-	IAT lily = new Human("Lily",this.marilyn,this.carlos);
-	
+  IAT unknown = new Unknown();
+  IAT catalina = new Human("Catalina",this.unknown,this.unknown);
+  IAT pedro = new Human("Pedro",this.unknown,this.unknown);
+  IAT magdalena = new Human("Magdalena",this.unknown,this.unknown);
+  IAT jose = new Human("Jose",this.unknown,this.unknown);	
+  IAT marilyn = new Human("Marilyn",this.catalina,this.pedro);
+  IAT carlos = new Human("Carlos",this.magdalena,this.jose);
+  IAT lily = new Human("Lily",this.marilyn,this.carlos);	
 }
